a865398ebb359f8a3b333cb16b2abc03b71e9ab9
%%GITNAME%% <%%GITEMAIL%%>
2018-05-19 22:39:14 +0800

    regular commit

--- src/foo/bar/Utilities.groovy
+++ src/foo/bar/Utilities.groovy
@@ -1,12 +1,6 @@
 package foo.bar
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-
-  def pwd() {å
-    script.sh "pwd"
+class Utilities {
+  static def mvn(script, args) {
+    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
   }
 }
