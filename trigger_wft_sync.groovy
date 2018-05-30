01755eafa39d443384cdb5a5b5aabd630bebe8ea
%%GITNAME%% <%%GITEMAIL%%>
2018-05-30 22:55:02 +0800

    test for debug

---
+++ trigger_wft_sync.groovy
@@ -0,0 +1,5 @@
+#!/user/bin/env groovy
+
+"echo ${stream_version}-${branch}".execute()
+"/usr/local/bin/wget -nc $JENKINS_URL:8080/jnlpJars/jenkins-cli.jar".execute()
+"java -jar jenkins-cli.jar -s  $JENKINS_URL build ${stream_version}-${branch} -v -s -f".execute()
