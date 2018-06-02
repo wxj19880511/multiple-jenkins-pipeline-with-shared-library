fff140f4f821b5d1a87e5fe22e5c5b112df250f6
%%GITNAME%% <%%GITEMAIL%%>
2018-06-02 22:35:54 +0800

    test for debug

--- dsl_jobs/simple_dsl.groovy
+++ dsl_jobs/simple_dsl.groovy
@@ -1,7 +1,7 @@
 multibranchPipelineJob('example') {
     branchSources {
         git {
-            remote('https://github.com/wxj19880511/multiple-jenkins-pipeline-with-shared-library.gi')
+            remote('https://github.com/wxj19880511/multiple-jenkins-pipeline-with-shared-library.git')
             credentialsId('github')
             includes('*')
         }
