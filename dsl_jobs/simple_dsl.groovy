4f9048a2fee6ad2118867124dda684f0cea7e707
%%GITNAME%% <%%GITEMAIL%%>
2018-06-02 23:00:45 +0800

    test for debug

--- dsl_jobs/simple_dsl.groovy
+++ dsl_jobs/simple_dsl.groovy
@@ -3,7 +3,7 @@ multibranchPipelineJob('example') {
         git {
             remote('https://github.com/wxj19880511/multiple-jenkins-pipeline-with-shared-library.git')
             credentialsId('github')
-            includes('*')
+            includes('* v*')
         }
     }
     orphanedItemStrategy {
